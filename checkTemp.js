0b13cf71d89d08a4e76fa92c8409b362dec6d979
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-10 10:50:27 +0300

    Create checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,23 @@
+/* liiga külm 0-20
+okei 21-40
+liiga kuum
+*/
+function checkTemp() {
+  let temp=25
+
+if(temp<20){
+console.log("liiga"+" "+"külm");
+
+}if(temp>40){
+console.log("liiga kuum");
+
+}else if(temp>20 && temp<=40)
+console.log("Väga hea")
+
+
+}
+
+
+undefined
+checkTemp()
+VM306:15 Väga hea
