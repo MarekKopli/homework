0b65afd183f8f2a902099e51187d2ef9ff6d207f
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-24 12:15:04 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,13 +1,13 @@
 function checkTemp(temp) {
 
 
-if(temp<20){
+if(temp<21){
 console.log("liiga"+" "+"külm");
 return -1;
 }if(temp>40){
 console.log("liiga kuum");
 return +1;
-}else if(temp=>20 && temp<=40)
+}else if(temp=>21 && temp<=40)
 console.log("Väga hea")
 return 0;
 
